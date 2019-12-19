edc929b358491eb4826c765606c561b4063c544d
刘王权 <15586183797@163.com>
2019-12-19 21:13:33 +0800

    added GrammaticalAnalysis.cpp,GrammaticalAnalysis.h

---
+++ SimpleDatabaseKernel/GrammaticalAnalysis.cpp
@@ -0,0 +1,3 @@
+#ifndef _GrammaticalAnalysis_
+#include"GrammaticalAnalysis.h"
+#endif
---
+++ SimpleDatabaseKernel/GrammaticalAnalysis.h
@@ -0,0 +1,10 @@
+#include<iostream>
+#ifndef _TESTSWITCH_
+#include"testswitch.h"
+#include"file.h"
+#endif
+#define _GrammaticalAnalysis_
+class GrammaticalAnalysis {
+
+};
+
