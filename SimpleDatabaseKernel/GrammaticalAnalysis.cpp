13b299300722d475db5df4d6229408665edfd0ba
WangQuanLiu <WangQuanLiu@users.noreply.github.com>
2019-12-20 16:45:23 +0800

    added nfa,alphabet

--- SimpleDatabaseKernel/GrammaticalAnalysis.cpp
+++ SimpleDatabaseKernel/GrammaticalAnalysis.cpp
@@ -1,3 +1,22 @@
 #ifndef _GrammaticalAnalysis_
 #include"GrammaticalAnalysis.h"
 #endif
+const int GrammaticalAnalysis::nfa[NFA_MAX]{
+
+
+
+
+
+
+
+
+
+};
+const string GrammaticalAnalysis::alphabet[ALPHA_MAX]{
+
+	"use","database",""
+
+
+
+
+};
--- SimpleDatabaseKernel/GrammaticalAnalysis.h
+++ SimpleDatabaseKernel/GrammaticalAnalysis.h
@@ -4,7 +4,10 @@
 #include"file.h"
 #endif
 #define _GrammaticalAnalysis_
+#define NFA_MAX 34
+#define ALPHA_MAX 40
 class GrammaticalAnalysis {
-
+	static const int nfa[NFA_MAX];
+	static const string alphabet[ALPHA_MAX];
 };
 
