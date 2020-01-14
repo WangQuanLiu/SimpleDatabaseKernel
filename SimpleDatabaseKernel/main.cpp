f623195f112c01240515e16b18dc3256c2f43e58
刘王权 <15586183797@163.com>
2020-01-14 22:04:34 +0800

    do nothing

--- SimpleDatabaseKernel/main.cpp
+++ SimpleDatabaseKernel/main.cpp
@@ -21,7 +21,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 
-Project address :https://github.com/WangQuanLiu/SimpleDatabaseKernel
+The project address :https://github.com/WangQuanLiu/SimpleDatabaseKernel
 */
 #ifndef _LexicalAnalysis_
 #include"LexicalAnalysis.h"
